0fb0076e69914272d7e3002a8762fff0a10181d6
Asikul Islam Sazzat <asikulislamsazzat@gmail.com>
2025-05-19 11:10:01 +0600

    working with navbar

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,15 @@
-/** @type {import('tailwindcss').Config} */
+
 module.exports = {
   content: ["*"],
   theme: {
     extend: {
-      
+      colors:{
+'primary':'#3238f2',
+      },
+   fontFamily:{
+      'display':["Poppins", 'sans-serif'],
+      'body':['Inter', 'sans-serif']
+   }
     },
   },
   plugins: [],
